82edf03210a5bac9b848274cc915d8fd233c78e1
Ло П <90679190+luopinyao579@users.noreply.github.com>
2022-01-10 03:10:34 +0800

    Update Obfuscator.java

--- src/app/Obfuscator.java
+++ src/app/Obfuscator.java
@@ -4,7 +4,6 @@ public class Obfuscator {
     private static String inpString = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
     private static String outString = "kiMd6GvVlzUPwjg9734pSXbEqC5ZFOQxHyfIN8Bn0LoTA2rKaRhsmJtce1WYDu";
 
-
     public static String obf(String s) {
         char[] res = new char[s.length()];
         for (int i = 0; i < s.length(); i++) {
